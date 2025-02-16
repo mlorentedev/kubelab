4baca52545050758c6f6b4ef526af765305550c5
manu <manloralm@outlook.com>
2025-02-15 19:13:22 -0700

    Fix issue with static paths generation for posts and projects

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,9 +2,7 @@ import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
   loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-  // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
     description: z.string(),
@@ -15,7 +13,6 @@ const blog = defineCollection({
 });
 
 const projects = defineCollection({
-  // Load Markdown and MDX files in the `src/content/projects/` directory.
   loader: glob({ base: './src/content/projects', pattern: '**/*.{md,mdx}' }),
   schema: z.object({
     title: z.string(),
